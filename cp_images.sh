f98671bcc8e1a5b6e968816865d3063d2b74cbe2
Viktor Walter <viktor_walter@outlook.com>
2021-07-15 14:47:38 +0200

    Adding automatic imate copy generator

---
+++ cp_images.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $# == "0" ]]
+then
+  echo "Provide the camera number!"
+  exit
+fi
+
+rm i_*.jpg
+num=$1
+for f in $(ls ~/bag_files/uvdar_calib/${num}/*.jpg)
+do
+  cp $f ./i_$( basename ${f} ).jpg
+done
